049ed5f70e45ed017890d3894b23b73da8621989
Yonis Abdi <yonisabdi.jobb@gmail.com>
2024-01-08 14:15:25 +0100

    Initial commit

---
+++ WebApplication3/WebApplication3/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication3.Models;
+
+namespace WebApplication3.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void GetAll();
+        void GetFirstOrDefault();
+        void Update(Product obj);
+    }
+}
